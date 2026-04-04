a1e48d947f57a7056faaede34eac5fa935a5827f
trenerszb4-prog <trener.szb4@gmail.com>
2026-04-05 00:26:07 +0300

    speed

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Health-check
 app.get('/', (req, res) => {
